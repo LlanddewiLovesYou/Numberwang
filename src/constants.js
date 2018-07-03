ce7396dbf0e1580c243fe27954eb044fac22432c
Ian Del Duca <ianmdelduca@gmail.com>
2018-07-03 03:47:07 -0400

    djfsk

---
+++ src/constants.js
@@ -0,0 +1,61 @@
+export const OPENING = [
+  "Hello and welcome to Numberwang!",
+  "The maths quiz that's simply everyone!",
+  "Players, Enter your names and hometowns",
+  "and let's play NumberWang!"
+];
+
+export const WANGERNUMB = [
+  "It's time for WangerNumb!",
+  "Let's rotate the board!"
+]
+
+export const CLOSING = [
+  'Oh that\'s bad luck Julie',
+  'You\'ve been WangerNumbed!',
+  "But Simon, You are Today\'s Numberwang!",
+  "That\'s all from NumberWang",
+  'But until tomorrow\'s edition',
+  'Good Numberwang, and Remember:',
+  'Stay Numberwang!'
+]
+
+export const PHRASES = [
+  "That\'s Numberwang!",
+  "That\'s Numberwang!",
+  "Das ist NümberWang!",
+  "That\'s Numberwang!",
+  "That\'s Numberwang!",
+  "Unwang!",
+  "That\'s Numberwang!",
+  "That\'s Numberwang!",
+  "That\'s NumberFlan!",
+  "That\'s Numberwang!",
+  "That\'s Numberwang!",
+  'That\'s Yumberwang!',
+  "That\'s Numberwang!",
+  "That\'s Numberwang!",
+  'Belchwang!',
+  "That\'s Numberwang!",
+  "That\'s Numberwang!",
+  'That\'s the Prime Numberwang!',
+  "That\'s Numberwang!",
+  "That\'s Numberwang!",
+  'Wordwang!',
+  "That\'s Numberwang!",
+  "That\'s Numberwang!",
+  'That\'s NumberWank!',
+  "That\'s Numberwang!",
+  'Hooraywang!',
+  "That\'s Numberwang!",
+  "That\'s Numberwang!",
+]
+
+export const HOMETOWNS = [
+  ['Somerset', 'Somerset'],
+  ['Northampton', 'Southampton'],
+  ['Anglesea', 'Anglesea'],
+  ['Ein Hamburger', 'Ein Frankfurter'],
+  ['Yorkshire', 'a factory and made of a special metal'],
+  ['Durham', 'Space']
+]
